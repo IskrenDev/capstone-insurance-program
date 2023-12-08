8b7182fd4f6bdd37a9cbeac3160f74c6a6863b4b
iskrenradev <iskrenr005@gmail.com>
2023-12-08 02:40:44 +0100

    Move all props to types.ts for better code organization

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -12,15 +12,46 @@ export type Insurance = {
     paymentPerMonth:number,
     startDate:string,
     endDate:string,
-    hasHealthIssues:boolean,
-    healthConditionDetails: string
-    propertyType: string,
-    propertyAddress: string,
-    constructionYear: number,
-    vehicleMake: string,
-    vehicleModel: string;
-    vehicleYear: number;
-    licensePlateNumber: string
+    hasHealthIssues?:boolean,
+    healthConditionDetails?: string
+    propertyType?: string,
+    propertyAddress?: string,
+    constructionYear?: number,
+    vehicleMake?: string,
+    vehicleModel?: string;
+    vehicleYear?: number;
+    licensePlateNumber?: string
+};
+
+export type FormLabelProps = {
+    label: string;
+    name: string;
+    value?: string | number | readonly string[]
+    handleOnChangeNumber?: (v: number) => void;
+    handleOnChangeText?: (v: string) => void;
+    handleOnChangeCheckbox?: (v: boolean) => void;
+    handleOnChangeDate?: (v: string) => void;
+    isRequired?: boolean;
+    checked?: boolean
+    options?: { value: string; label: string }[];
+    textarea?: boolean;
+    pattern?: string;
+    startDate?: string;
+    endDate?: string;
+    type?: "date" | "number" | "checkbox" | "text" | "email" | "tel";
+}
+
+export type DetailsLabelProps = {
+    label: string;
+    value?: string | number | boolean;
+    insuranceType?: { value: string; label: string }[];
+    type?: "email" | "tel" | "text";
+};
+
+export type InsuranceListProps = {
+    insurances: Insurance[];
+    headerText: string;
+    type: string;
 };
 
 export type AllInsurancesResponse = {
