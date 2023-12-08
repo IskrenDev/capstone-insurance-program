5a246edbea1ddf1b91f980cfa42bfb8baf2021da
iskrenradev <iskrenr005@gmail.com>
2023-12-08 16:36:46 +0100

    Edit header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,11 +1,14 @@
 import "./Header.css";
+import "../../assets/HomeSvg.css"
 import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <header className={"header-border"}>
             <nav className={"nav-main"}>
-                <Link to={"/"}>Startseite</Link>
+                <Link to={"/"}>
+                    <a href="/" className="svg-icon"></a>
+                </Link>
             </nav>
         </header>
     );
