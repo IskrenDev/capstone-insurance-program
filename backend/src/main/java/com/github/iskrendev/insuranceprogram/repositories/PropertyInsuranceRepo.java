af61834b78c49f48abd7231bde250c99a9afe50d
Iskren Radev <iskrenradev003@gmail.com>
2024-01-22 23:29:18 +0100

    Update LifeInsuranceRepo, PropertyInsuranceRepo and VehicleInsuranceRepo to handle case-insensitive queries

--- backend/src/main/java/com/github/iskrendev/insuranceprogram/repositories/PropertyInsuranceRepo.java
+++ backend/src/main/java/com/github/iskrendev/insuranceprogram/repositories/PropertyInsuranceRepo.java
@@ -2,14 +2,18 @@ package com.github.iskrendev.insuranceprogram.repositories;
 
 import com.github.iskrendev.insuranceprogram.models.PropertyInsurance;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PropertyInsuranceRepo extends MongoRepository <PropertyInsurance, String> {
+    @Query("{ 'firstName': { $regex: ?0, $options: 'i' } }")
     List<PropertyInsurance> findByFirstName(String firstName);
+    @Query("{ 'familyName': { $regex: ?0, $options: 'i' } }")
     List<PropertyInsurance> findByFamilyName(String familyName);
+    @Query("{ 'firstName': { $regex: ?0, $options: 'i' }, 'familyName': { $regex: ?1, $options: 'i' } }")
     List<PropertyInsurance> findByFirstNameAndFamilyName(String firstName, String familyName);
 }
 
--- backend/src/main/java/com/github/iskrendev/insuranceprogram/repositories/VehicleInsuranceRepo.java
+++ backend/src/main/java/com/github/iskrendev/insuranceprogram/repositories/VehicleInsuranceRepo.java
@@ -2,13 +2,17 @@ package com.github.iskrendev.insuranceprogram.repositories;
 
 import com.github.iskrendev.insuranceprogram.models.VehicleInsurance;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface VehicleInsuranceRepo extends MongoRepository <VehicleInsurance, String> {
+    @Query("{ 'firstName': { $regex: ?0, $options: 'i' } }")
     List<VehicleInsurance> findByFirstName(String firstName);
+    @Query("{ 'familyName': { $regex: ?0, $options: 'i' } }")
     List<VehicleInsurance> findByFamilyName(String familyName);
+    @Query("{ 'firstName': { $regex: ?0, $options: 'i' }, 'familyName': { $regex: ?1, $options: 'i' } }")
     List<VehicleInsurance> findByFirstNameAndFamilyName(String firstName, String familyName);
 }
